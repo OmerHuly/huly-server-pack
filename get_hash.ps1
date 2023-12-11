c9fbe1e85cf24345480d012e48e61da7c27b35c8
Omer Huly <16382306+OmerHuly@users.noreply.github.com>
2023-12-11 22:09:46 +0200

    Use 7zip

--- get_hash.ps1
+++ get_hash.ps1
@@ -1,8 +1,15 @@
 $zipFile = ".\huly-server-pack.zip"
-$compress = @{
-LiteralPath = ".\pack.mcmeta", ".\pack.png", ".\assets"
-DestinationPath = $zipFile
+$7zipPath = "E:\7Zip\7z.exe"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+    throw "7 zip file '$7zipPath' not found"
 }
-Compress-Archive @compress -Update
+
+Set-Alias Start-SevenZip $7zipPath
+
+$Source = "c:\BackupFrom\backMeUp.txt"
+
+Start-SevenZip a -mx=9 "$zipFile" "assets" "pack.mcmeta" "pack.png"
+
 $FileHash = (Get-FileHash -Algorithm SHA1 $zipFile).Hash.toLower()
 Write-Output $FileHash
